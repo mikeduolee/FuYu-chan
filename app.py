f45c784e3086024bc62feafb54717a3455a6ed15
mikeduolee <36064126+mikeduolee@users.noreply.github.com>
2025-03-28 15:02:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
+print("Access token:", os.getenv("CHANNEL_ACCESS_TOKEN"))
+print("Secret:", os.getenv("CHANNEL_SECRET"))
+
 app = Flask(__name__)
 
 line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
